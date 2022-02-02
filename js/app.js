426d110d33ed6275272caaaec96fccaeaa89529c
catherinetung <cattkaizen@gmail.com>
2022-02-02 18:58:12 -0500

    One solution to create list then create reset

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-console.log('sanity check')
+const inp =  document.querySelector('input')
+const btn = document.querySelector('button')
+const list = document.querySelector('ul')
+
+btn.addEventListener('click', function(evt){
+  const newLi = document.createElement('li')
+  const inp = document.querySelector('input')
+  newLi.textContent = inp.value
+  const rst = document.getElementById('reset')
+  rst.addEventListener('click', function(evt2){
+    document.getElementById('todo-list').removeChild(newLi)
+    input.value = ""
+  })
+  
+  if (inp.value !== "") {
+    document.querySelector('ul').appendChild(newLi)
+    inp.value = ""
+  }
+})
+
