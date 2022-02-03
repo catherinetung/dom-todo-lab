8b6f52a2ac7351947f465fa40029b9d35ae56fa6
catherinetung <cattkaizen@gmail.com>
2022-02-02 19:18:17 -0500

    New solution for reset button looping through all nodes

--- js/app.js
+++ js/app.js
@@ -6,15 +6,16 @@ btn.addEventListener('click', function(evt){
   const newLi = document.createElement('li')
   const inp = document.querySelector('input')
   newLi.textContent = inp.value
-  const rst = document.getElementById('reset')
-  rst.addEventListener('click', function(evt2){
-    document.getElementById('todo-list').removeChild(newLi)
-    input.value = ""
-  })
-  
   if (inp.value !== "") {
     document.querySelector('ul').appendChild(newLi)
     inp.value = ""
   }
+})
+
+const rst = document.getElementById('reset')
+rst.addEventListener('click', function(evt2){
+  const oldList = document.querySelectorAll('li');
+  oldList.forEach(todo => todo.remove())
+  input.value = ""
 })
 
